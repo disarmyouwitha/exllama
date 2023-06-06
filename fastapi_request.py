e2d15e114bafb53cefc47d8a642e38f02e37f3d2
disarmyouwitha <disarmyouwitha@gmail.com>
2023-06-06 18:27:09 -0500

    update example

--- fastapi_request.py
+++ fastapi_request.py
@@ -5,11 +5,18 @@ data = {
     "prompt": "You are Samantha, a sentient AI.\nUSER: {user_input}\nASSISTANT:",
     "message": "Tell me about yourself!",
     "max_new_tokens": 256
+    # [DEFAULTS]:
+    #temperature: 0.7,
+    #top_k: 20,
+    #top_p: 0.65,
+    #min_p: 0.06,
+    #token_repetition_penalty_max: 1.15,
+    #token_repetition_penalty_sustain: 256,
+    #token_repetition_penalty_decay: 128,
+    #stream: true,
 }
 
 r = requests.post("http://localhost:7862/generate", json=data, stream=True)
-#r = requests.post("http://wintermute:7862/generate", json=data, stream=True)
-#r = requests.post("http://localhost:8000/ask", json=data, stream=True)
 
 if r.status_code==200:
     for chunk in r.iter_content():
